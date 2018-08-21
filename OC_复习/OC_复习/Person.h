ef79f979deaff40aa96ffbf5afef25fd81db25a2
MZP <604183841@qq.com>
2018-08-21 22:30:10 +0800

    第一天的内容

---
+++ OC_复习/OC_复习/Person.h
@@ -0,0 +1,17 @@
+//
+//  Person.h
+//  OC_复习
+//
+//  Created by 毛志鹏 on 2018/8/21.
+//  Copyright © 2018年 毛志鹏. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@property (nonatomic, readonly) NSString *name;
+@property (nonatomic, copy) NSString *schoolName;
+
+
+@end
--- OC_复习/OC_复习/main.m
+++ OC_复习/OC_复习/main.m
@@ -8,6 +8,19 @@
 
 #import <Foundation/Foundation.h>
 
+@interface Ss: NSObject {
+    @public
+    NSString *_name;
+    int _age;
+    float _height;
+}
+
+@end
+
+@implementation Ss
+
+@end
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
@@ -15,6 +28,12 @@ int main(int argc, const char * argv[]) {
         
         // NSString 复习
         NSString *string = @"abcdefghijklmn";
+        NSLog(@"%@", string);
+        
+        Ss *p1 = [Ss new];
+        p1->_name = @"jack";
+        NSLog(@"p1的名字是%@", p1->_name);
+        
         
         
         
