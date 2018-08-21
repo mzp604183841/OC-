5219f8eeac5249817986a95f8eab6c2ba75aa384
MZP <604183841@qq.com>
2018-08-21 09:23:37 +0800

    新建OC复习工程

---
+++ OC_复习/OC_复习/main.m
@@ -0,0 +1,20 @@
+//
+//  main.m
+//  OC_复习
+//
+//  Created by 毛志鹏 on 2018/8/21.
+//  Copyright © 2018年 毛志鹏. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+        
+        
+    }
+    return 0;
+}
