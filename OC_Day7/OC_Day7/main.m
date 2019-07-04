19192966c2b3cf3cdffd28d2c2231be9cbd836fd
MZP <604183841@qq.com>
2019-07-04 10:09:30 +0800

    我也不知道写了些啥，先提交

---
+++ OC_Day7/OC_Day7/main.m
@@ -0,0 +1,36 @@
+//
+//  main.m
+//  OC_Day7
+//
+//  Created by 毛志鹏 on 2019/2/13.
+//  Copyright © 2019 毛志鹏. All rights reserved.
+//
+
+
+
+/*
+ 
+    arc模式下
+    strong 大多数情况下
+    weak    当出现循环引用的时候
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ */
+
+
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    
+    }
+    return 0;
+}
